0c246df4d51bceab8de149a4e6be7753cc64ef88
bestcoolestp <108534975+bestcoolestp@users.noreply.github.com>
2023-05-15 17:08:13 +0900

    feedback interface
    feedback UI with HTML CSS JS

---
+++ Review Interface/index.js
@@ -0,0 +1,34 @@
+const ratingEls = document.querySelectorAll('.rating');
+const btnEl = document.getElementById('btn');
+
+const containerEl = document.getElementById('container');
+
+let selectedRating = "";
+
+ratingEls.forEach(ratingEl => {
+    ratingEl.addEventListener('click', (event) => {
+        removeActive();
+        selectedRating = event.target.innerText || event.target.parentNode.innerText;
+        event.target.classList.add('active');
+        event.target.parentNode.classList.add('active');
+    });
+});
+
+function removeActive() {
+    ratingEls.forEach(ratingEl => {
+        ratingEl.classList.remove('active');
+        ratingEl.parentNode.classList.remove('active');
+    });
+}
+
+btn.addEventListener('click', () => {
+    if(selectedRating !== "") {
+        containerEl.innerHTML =  `
+        <strong>Thank you!</strong>
+        <br>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer service.</p>
+        `;
+    }
+});
